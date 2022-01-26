192554114dc4112a53b4e3d43b1faa99e4c36e61
Callan <cerveauxallanjean@gmail.com>
2022-01-26 17:40:56 +0100

    TEST: Test if bill icon as highlighted when user is on bill page

--- front/src/__tests__/Bills.js
+++ front/src/__tests__/Bills.js
@@ -2,16 +2,30 @@
  * @jest-environment jsdom
  */
 
-import { screen } from "@testing-library/dom"
-import BillsUI from "../views/BillsUI.js"
-import { bills } from "../fixtures/bills.js"
+import { screen } from "@testing-library/dom";
+import Router from '../app/Router';
+import { ROUTES_PATH } from '../constants/routes';
+import { bills } from "../fixtures/bills.js";
+import BillsUI from "../views/BillsUI.js";
+import { localStorageMock } from '../__mocks__/localStorage';
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
     test("Then bill icon in vertical layout should be highlighted", () => {
-      const html = BillsUI({ data: []})
-      document.body.innerHTML = html
-      //to-do write expect expression
+      Object.defineProperty(window, "localStorage", {
+        value: localStorageMock
+      })
+      window.localStorage.setItem(
+        'user',
+        JSON.stringify({
+          type: "Employee"
+        })
+      )
+      const pathname = ROUTES_PATH['Bills'];
+      Object.defineProperty(window, 'location', { value: { hash: pathname } })
+      document.body.innerHTML = '<div id="root"></div>'
+      Router();
+      expect(screen.getByTestId("icon-window").classList.contains('active-icon')).toBe(true)
     })
     test("Then bills should be ordered from earliest to latest", () => {
       const html = BillsUI({ data: bills })
